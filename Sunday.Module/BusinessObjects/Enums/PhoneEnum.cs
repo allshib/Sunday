5f60a047c5c6f8abb6d232fe76b056792088f8c2
al.shibalkin <al.shibalkin@ya.ru>
2023-06-03 16:46:16 +0500

    Добавление локализации

---
+++ Sunday.Module/BusinessObjects/Enums/PhoneEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sunday.Module.BusinessObjects.Enums {
+	public enum PhoneEnum {
+		None,
+		Mobile,
+		Work
+	}
+}
