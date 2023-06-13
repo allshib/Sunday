40df92735d9ba064acf3974e3eb34c6bffd87b2a
al.shibalkin <al.shibalkin@ya.ru>
2023-06-13 18:27:37 +0500

    Добавлена древовидность

---
+++ Sunday.Module/BusinessObjects/SundayDataModelCode/BaseTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base.General;
+
+namespace Sunday.Module.BusinessObjects.SundayDataModel {
+
+    public partial class BaseTree : ITreeNode {
+        public BaseTree(Session session) : base(session) { }
+
+        public IBindingList Children => BaseTreeCollection;
+
+        ITreeNode ITreeNode.Parent => this.Parent;
+
+        string ITreeNode.Name => this.Name;
+
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
